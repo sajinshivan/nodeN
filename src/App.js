57fb51c239d43b8b721d519ef93f1b3406ee5463
sajin <codemesj@gmail.com>
2024-12-08 17:39:34 +0530

    learned Get,Post,Put,Delete Methods

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ const express = require("express");
 
 const app = express();
 
+app.post("/user", (req,res) => {
+  res.send({ firstname: "sajin", lastname: "sivan" });
+});
+app.get("/user", (req,res) => {
+  res.send("successfully saved");
+});
+app.delete("/user", (req,res) => {
+  res.send("successfully deleted");
+});
+app.put("/user", (req,res) => {
+  res.send("successfully updated");
+});
 app.use("/test", (req, res) => {
   res.send("Hello there");
 });
 app.use("/world", (req, res) => {
-  res.send("Hello world");
+  res.send("Hell world");
 });
 app.listen(7777, () => {
   console.log("hello thre 77");
